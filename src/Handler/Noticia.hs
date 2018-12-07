4f100bce50683b1d98516df3aa1cee2baf98abb8
gabriel.fernando033@gmail.com <gabriel.fernando033@gmail.com>
2018-12-07 18:41:47 +0000

    Inserir noticia

--- src/Handler/Noticia.hs
+++ src/Handler/Noticia.hs
@@ -12,3 +12,11 @@ getListaNoticiaR :: Handler TypedContent
 getListaNoticiaR = do 
     noticias <- runDB $ selectList [] [Desc NoticiaId]
     sendStatusJSON ok200 (object ["noticias" .= noticias])
+    
+-- Post noticia
+postNoticiaR :: Handler TypedContent
+postNoticiaR = do 
+    addHeader "ACCESS-CONTROL-ALLOW-ORIGIN" "*"
+    noticia <- requireJsonBody :: Handler Noticia
+    noticiaid <- runDB $ insert noticia
+    sendStatusJSON created201 (object ["noticia" .= noticia])
