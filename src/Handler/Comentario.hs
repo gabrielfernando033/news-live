77f938c1bf670f4c1760bb0c26cc57ca8eddc1d1
gabriel.fernando033@gmail.com <gabriel.fernando033@gmail.com>
2018-12-09 17:06:32 +0000

    Criando os models e as rotas para os comentarios

---
+++ src/Handler/Comentario.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Handler.Comentario where
+
+import Import
