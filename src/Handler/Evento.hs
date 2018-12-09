0640cdf0490b92116577475b11e8e619ee045653
gabriel.fernando033@gmail.com <gabriel.fernando033@gmail.com>
2018-12-09 18:41:06 +0000

    Adicionando endpoints de inserção e busca de eventos

--- src/Handler/Evento.hs
+++ src/Handler/Evento.hs
@@ -6,3 +6,17 @@
 module Handler.Evento where
 
 import Import
+
+-- Get all eventos
+getListaEventoR :: Handler TypedContent
+getListaEventoR = do 
+    eventos <- runDB $ selectList [] [Desc EventoId]
+    sendStatusJSON ok200 (object ["eventos" .= eventos])
+    
+-- Post evento
+postEventoR :: Handler TypedContent
+postEventoR = do 
+    addHeader "ACCESS-CONTROL-ALLOW-ORIGIN" "*"
+    evento <- requireJsonBody :: Handler Evento
+    _ <- runDB $ insert evento
+    sendStatusJSON created201 (object ["evento" .= evento])
